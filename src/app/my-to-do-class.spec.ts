30719ab4034d4bd95b94243ba90219aec9babe61
DavidKibetRono <david.rono@student.moringaschool.com>
2022-01-15 17:49:30 +0300

    add to-do app

---
+++ src/app/my-to-do-class.spec.ts
@@ -0,0 +1,7 @@
+import { MyToDoClass } from './my-to-do-class';
+
+describe('MyToDoClass', () => {
+  it('should create an instance', () => {
+    expect(new MyToDoClass()).toBeTruthy();
+  });
+});
