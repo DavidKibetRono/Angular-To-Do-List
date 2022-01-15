d0ca38330ffd25f6b40402f23c6885d30226b13d
DavidKibetRono <david.rono@student.moringaschool.com>
2022-01-15 17:47:58 +0300

    add the logic component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
-
+import { MyToDoClass } from './my-to-do-class';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TO-DO';
+  // inputs!:any;
+  mytodos:MyToDoClass[]=[
+    {
+      content:"PHP",
+      completed:false
+    },
+    {
+      content:"Java",
+      completed:false
+    },
+
+    {
+      content:"javaScript",
+      completed:false
+    },
+    {
+      content:"Python",
+      completed:false
+    }
+  ]
+
+  strikeContent(id:number){
+    this.mytodos.map((value,index)=>{
+      if(index===id) {
+        value.completed=!value.completed
+      }
+      return value
+    })
+  }
+
+  deleteContent(id:number){
+    this.mytodos=this.mytodos.filter((value,index)=>index!==id)
+  }
+  
+  addList(data:any){
+
+    this.mytodos.push({
+      content:data,
+      completed:false
+    });
+
+    // this.inputs="";
+
+  }
 }
